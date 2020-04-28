01943d1c3f4ca5826297b9d670af357a542a29d1
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-04-28 01:23:02 +0100

    adding admin

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ module.exports = function (eleventyConfig) {
   // Output directory: _site
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/js");;
+  eleventyConfig.addPassthroughCopy("src/admin");
 
   eleventyConfig.addCollection("portfolioReversed", collection => {
     return collection.getFilteredByTag("portfolio").reverse();
