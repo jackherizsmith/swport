2cecd7feedf4499bdec3aea055c59172adbdc357
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-07-20 20:53:22 +0100

    add nav buttons to article

--- .eleventy.js
+++ .eleventy.js
@@ -9,8 +9,19 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/_redirects");
 
   eleventyConfig.addCollection("portfolioReversed", collection => {
+    const coll = collection.getFilteredByTag("portfolio");
+
+    for(let i = 0; i < coll.length ; i++) {
+      const prevPost = coll[i-1];
+      const nextPost = coll[i+1];
+
+      coll[i].data["prevPost"] = prevPost;
+      coll[i].data["nextPost"] = nextPost;
+    }
     return collection.getFilteredByTag("portfolio").reverse(); // filter by featured
   });
+
+
 
   return {
     dir: {
