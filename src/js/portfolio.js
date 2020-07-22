188086c7b8f9f7e8d086af651aae85c9ab287ac8
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-07-22 13:30:04 +0100

    remove multipage history

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,1 +1,14 @@
 //prevent layering history
+const links = document.querySelectorAll("a");
+
+
+function pushNewHistory(event){
+    event.preventDefault();
+    location.replace(event.target.href)
+}
+
+links.forEach(link => {
+    if (link.href.includes("portfolio/articles")){
+        link.addEventListener("click", pushNewHistory);
+    }
+});
